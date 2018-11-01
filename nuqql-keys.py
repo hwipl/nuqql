a9dbb8a82fa7f64f88d65001c2e0e16b525a885c
hwipl <33433250+hwipl@users.noreply.github.com>
2018-11-01 23:33:25 +0100

    add nuqql-keys.py
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

---
+++ nuqql-keys.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import curses
+import curses.ascii
+
+def main(stdscr):
+    while True:
+        ch = stdscr.get_wch()
+        if type(ch) is not str:
+            msg = "int: " + str(ch) + "\n"
+        else:
+            msg = "char: " + ch
+            msg += " ord: " + str(ord(ch))
+            msg += " ascii: " + curses.ascii.ascii(ch)
+            msg += " crtl: " + curses.ascii.ctrl(ch)
+            msg += " alt: " + curses.ascii.alt(ch) + "\n"
+        stdscr.addstr(msg)
+
+curses.wrapper(main)
