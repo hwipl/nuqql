acaf47c5134cd4869188d5b5824dc1c125f12deb
hwipl <33433250+hwipl@users.noreply.github.com>
2019-07-09 16:44:35 +0200

    add more color pair definitions to color config
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- nuqql/config.py
+++ nuqql/config.py
@@ -95,22 +95,26 @@ class WinConfig:
 
         # allow usage of default colors and initialize color pairs
         curses.use_default_colors()
-        curses.init_pair(1, curses.COLOR_BLUE, -1)
-        curses.init_pair(2, curses.COLOR_GREEN, -1)
-        curses.init_pair(3, curses.COLOR_YELLOW, -1)
-        curses.init_pair(4, curses.COLOR_CYAN, -1)
+        curses.init_pair(1, curses.COLOR_BLACK, -1)
+        curses.init_pair(2, curses.COLOR_BLUE, -1)
+        curses.init_pair(3, curses.COLOR_CYAN, -1)
+        curses.init_pair(4, curses.COLOR_GREEN, -1)
+        curses.init_pair(5, curses.COLOR_MAGENTA, -1)
+        curses.init_pair(6, curses.COLOR_RED, -1)
+        curses.init_pair(7, curses.COLOR_WHITE, -1)
+        curses.init_pair(8, curses.COLOR_YELLOW, -1)
 
         # set window colors/attributes
-        self.attr["win_border"] = curses.color_pair(1) | curses.A_BOLD
-        self.attr["list_win_text"] = curses.color_pair(2)
+        self.attr["win_border"] = curses.color_pair(2) | curses.A_BOLD
+        self.attr["list_win_text"] = curses.color_pair(4)
         self.attr["log_win_text_peer_old"] = \
-            curses.color_pair(3) | curses.A_NORMAL
+            curses.color_pair(8) | curses.A_NORMAL
         self.attr["log_win_text_peer_new"] = \
-            curses.color_pair(3) | curses.A_BOLD
+            curses.color_pair(8) | curses.A_BOLD
         self.attr["log_win_text_self_old"] = \
-            curses.color_pair(4) | curses.A_NORMAL
+            curses.color_pair(3) | curses.A_NORMAL
         self.attr["log_win_text_self_new"] = \
-            curses.color_pair(4) | curses.A_BOLD
+            curses.color_pair(3) | curses.A_BOLD
 
     def init_keymap(self):
         """
