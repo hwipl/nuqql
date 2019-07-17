ebd2ff3d9ee6e8ece9e8c3a7068d9abe18b15698
hwipl <33433250+hwipl@users.noreply.github.com>
2019-07-17 15:09:16 +0200

    nuqql-keys: add menu, output key numbers used by nuqql keymap
    Add a menu that allows quitting nuqql-keys and switching to key number
    printing mode. In key number printing mode, output the key numbers that
    nuqql uses in its keymap configuration.
    
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- tools/nuqql-keys.py
+++ tools/nuqql-keys.py
@@ -3,29 +3,88 @@
 import curses
 import curses.ascii
 
-CHAR = ""
+
+def go_key_numbers(win):
+    """
+    Read input key and ouput its number if possible
+    """
+
+    # output strings
+    key_fmt = "Key number: {}"
+    no_key_fmt = "No key number: {}"
+    abort_text = "Press CTRL-C to abort and return to menu\n"
+    enter_text = "<enter key>"
+
+    # clear old window content
+    win.clear()
+    win.addstr(abort_text)
+    win.addstr(enter_text)
+
+    while True:
+        # read next character until keyboard interrupt
+        try:
+            char = win.get_wch()
+        except curses.error:
+            continue
+        except KeyboardInterrupt:
+            return
+
+        # construct ouput message...
+        msg = ""
+        if isinstance(char, str):
+            try:
+                msg = key_fmt.format(ord(char))
+            except TypeError:
+                msg = no_key_fmt.format(char)
+        else:
+            msg = key_fmt.format(char)
+
+        # ... and print it
+        win.clear()
+        win.addstr(abort_text)
+        win.addstr(msg)
+
+
+def go_menu(win):
+    """
+    Print menu to screen
+    """
+
+    # text to display
+    menu_text = (
+        "Menu:",
+        "p)  print keys",
+        "q)  quit"
+    )
+
+    while True:
+        try:
+            # output menu
+            win.clear()
+            win.addstr("\n".join(menu_text))
+
+            # get user input
+            char = win.get_wch()
+        except curses.error:
+            continue
+
+        # handle user input
+        if char == "p":
+            go_key_numbers(win)
+        if char == "q":
+            return
 
 
 def main(stdscr):
-    while True:
-        global CHAR
-        CHAR = stdscr.get_wch()
-        ch = CHAR
-        if type(ch) is not str:
-            msg = "int: " + str(ch) + "\n"
-        else:
-            if ch == "\0":
-                continue
-            msg = "char: " + ch
-            msg += " ord: " + str(ord(ch))
-            msg += " ascii: " + curses.ascii.ascii(ch)
-            msg += " crtl: " + curses.ascii.ctrl(ch)
-            msg += " alt: " + curses.ascii.alt(ch) + "\n"
-        stdscr.addstr(msg)
+    """
+    Main function
+    """
+
+    try:
+        go_menu(stdscr)
+    except KeyboardInterrupt:
+        pass
 
 
-try:
+if __name__ == "__main__":
     curses.wrapper(main)
-except Exception as e:
-    print("last input: \"{}\"".format(CHAR))
-    print(e)
