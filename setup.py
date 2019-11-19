1be33d4b1a83d9a574746bccd934ff137b1fb059
hwipl <33433250+hwipl@users.noreply.github.com>
2019-11-19 22:41:13 +0100

    add setup.py
    Add initial version of setup.py.
    
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

---
+++ setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+"""
+nuqql setup file
+"""
+
+from setuptools import setup
+
+VERSION = "0.7"
+DESCRIPTION = "Command line instant messenger inspired by centericq/centerim"
+with open("README.md", 'r') as f:
+    LONG_DESCRIPTION = f.read()
+CLASSIFIERS = [
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+]
+
+setup(
+    name="nuqql",
+    version=VERSION,
+    description=DESCRIPTION,
+    license="MIT",
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    author="hwipl",
+    author_email="nuqql@hwipl.net",
+    url="https://github.com/hwipl/nuqql",
+    packages=["nuqql"],
+    entry_points={
+        "console_scripts": ["nuqql = nuqql.main:run"]
+    },
+    classifiers=CLASSIFIERS,
+    python_requires='>=3.6',
+)
