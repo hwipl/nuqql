c68ac2ff5f28b891cc53a4fdac4bdeaabf7d965d
hwipl <33433250+hwipl@users.noreply.github.com>
2019-12-15 14:06:32 +0100

    add typing to win.py
    Add initial typing to win.py.
    
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- nuqql/win.py
+++ nuqql/win.py
@@ -5,9 +5,14 @@ Nuqql UI Windows
 import curses
 
 from types import SimpleNamespace
+from typing import TYPE_CHECKING, Any, Callable, Dict, List
+
+if TYPE_CHECKING:   # imports for typing
+    from nuqql.config import WinConfig
+    from nuqql.conversation import Conversation
 
 # screen and main windows
-MAIN_WINS = {}
+MAIN_WINS: Dict[str, Any] = {}
 
 
 class Win:
@@ -15,7 +20,8 @@ class Win:
     Base class for Windows
     """
 
-    def __init__(self, config, conversation, title):
+    def __init__(self, config: "WinConfig", conversation: "Conversation",
+                 title: str) -> None:
         # configuration
         self.config = config
 
@@ -44,10 +50,10 @@ class Win:
         )
 
         # keymaps/bindings
-        self.keyfunc = {}
+        self.keyfunc: Dict[str, Callable] = {}
         self._init_keyfunc()
 
-    def list_add(self, internal_list, entry):
+    def list_add(self, internal_list: List, entry: Any) -> None:
         """
         Add entry to internal list
         """
@@ -69,7 +75,7 @@ class Win:
                 return
             self.redraw()
 
-    def _redraw_win(self):
+    def _redraw_win(self) -> None:
         """
         Redraw entire window
         """
@@ -100,7 +106,7 @@ class Win:
 
         self.win.refresh()
 
-    def _move_pad(self):
+    def _move_pad(self) -> None:
         """
         Move the pad
         """
@@ -127,7 +133,7 @@ class Win:
         if self.state.cur_y < self.state.pad_y:
             self.state.pad_y = self.state.cur_y
 
-    def _check_borders(self):
+    def _check_borders(self) -> None:
         """
         Check borders
         """
@@ -152,14 +158,14 @@ class Win:
         if self.state.pad_y + (win_size_y - 3) > pad_size_y:
             self.state.pad_y = pad_size_y - (win_size_y - 3)
 
-    def redraw_pad(self):
+    def redraw_pad(self) -> None:
         """
         Redraw pad in window
         """
 
         # implemented in other classes
 
-    def redraw(self):
+    def redraw(self) -> None:
         """
         Redraw the window
         """
@@ -167,7 +173,7 @@ class Win:
         self._redraw_win()
         self.redraw_pad()
 
-    def resize_win(self, win_y_max, win_x_max):
+    def resize_win(self, win_y_max: int, win_x_max: int) -> None:
         """
         Resize window
         """
@@ -179,7 +185,7 @@ class Win:
         # TODO: change function parameters?
         self.win.resize(win_y_max, win_x_max)
 
-    def move_win(self, pos_y, pos_x):
+    def move_win(self, pos_y: int, pos_x: int) -> None:
         """
         Move window
         """
@@ -190,154 +196,154 @@ class Win:
 
         self.win.mvwin(pos_y, pos_x)
 
-    def _go_back(self, *args):
+    def _go_back(self, *args: Any) -> None:
         """
         User input: go back
         """
 
         # implemented in sub classes
 
-    def _cursor_right(self, *args):
+    def _cursor_right(self, *args: Any) -> None:
         """
         User input: cursor right
         """
 
         # implemented in sub classes
 
-    def _cursor_left(self, *args):
+    def _cursor_left(self, *args: Any) -> None:
         """
         User input: cursor left
         """
 
         # implemented in sub classes
 
-    def _cursor_down(self, *args):
+    def _cursor_down(self, *args: Any) -> None:
         """
         User input: cursor down
         """
 
         # implemented in sub classes
 
-    def _cursor_up(self, *args):
+    def _cursor_up(self, *args: Any) -> None:
         """
         User input: cursor up
         """
 
         # implemented in sub classes
 
-    def _cursor_top(self, *args):
+    def _cursor_top(self, *args: Any) -> None:
         """
         User input: cursor top
         """
 
         # implemented in sub classes
 
-    def _cursor_bottom(self, *args):
+    def _cursor_bottom(self, *args: Any) -> None:
         """
         User input: cursor bottom
         """
 
         # implemented in sub classes
 
-    def _cursor_page_up(self, *args):
+    def _cursor_page_up(self, *args: Any) -> None:
         """
         User input: cursor page up
         """
 
         # implemented in sub classes
 
-    def _cursor_page_down(self, *args):
+    def _cursor_page_down(self, *args: Any) -> None:
         """
         User input: cursor page down
         """
 
         # implemented in sub classes
 
-    def _send_msg(self, *args):
+    def _send_msg(self, *args: Any) -> None:
         """
         User input: send message
         """
 
         # implemented in sub classes
 
-    def _delete_char(self, *args):
+    def _delete_char(self, *args: Any) -> None:
         """
         User input: delete character
         """
 
         # implemented in sub classes
 
-    def _delete_char_right(self, *args):
+    def _delete_char_right(self, *args: Any) -> None:
         """
         User input: delete character on the right of current cursor position
         """
 
         # implemented in sub classes
 
-    def _cursor_msg_start(self, *args):
+    def _cursor_msg_start(self, *args: Any) -> None:
         """
         User input: move cursor to message start
         """
 
         # implemented in sub classes
 
-    def _cursor_msg_end(self, *args):
+    def _cursor_msg_end(self, *args: Any) -> None:
         """
         User input: move cursor to message end
         """
 
         # implemented in sub classes
 
-    def _cursor_line_start(self, *args):
+    def _cursor_line_start(self, *args: Any) -> None:
         """
         User input: move cursor to line start
         """
 
         # implemented in sub classes
 
-    def _cursor_line_end(self, *args):
+    def _cursor_line_end(self, *args: Any) -> None:
         """
         User input: move cursor to line end
         """
 
         # implemented in sub classes
 
-    def _delete_line_end(self, *args):
+    def _delete_line_end(self, *args: Any) -> None:
         """
         User input: delete from cursor to end of current line
         """
 
         # implemented in sub classes
 
-    def _delete_line(self, *args):
+    def _delete_line(self, *args: Any) -> None:
         """
         User input: delete current line
         """
 
         # implemented in sub classes
 
-    def _zoom_win(self, *args):
+    def _zoom_win(self, *args: Any) -> None:
         """
         User input: zoom current window
         """
 
         # implemented in sub classes
 
-    def _zoom_win_url(self, *args):
+    def _zoom_win_url(self, *args: Any) -> None:
         """
         User input: zoom current window
         """
 
         # implemented in sub classes
 
-    def _go_log(self, *args):
+    def _go_log(self, *args: Any) -> None:
         """
         User input: go to the conversations log/history
         """
 
         # implemented in sub classes
 
-    def _go_next(self, *args):
+    def _go_next(self, *args: Any) -> None:
         """
         User input: go to conversation with new messages or more recently used
         conversation
@@ -345,63 +351,64 @@ class Win:
 
         # implemented in sub classes
 
-    def _go_prev(self, *args):
+    def _go_prev(self, *args: Any) -> None:
         """
         User input: go to previously used conversation
         """
 
         # implemented in sub classes
 
-    def _go_conv(self, *args):
+    def _go_conv(self, *args: Any) -> None:
         """
         User input: go to specific conversation
         """
 
         # implemented in sub classes
 
-    def _search(self, *args):
+    def _search(self, *args: Any) -> None:
         """
         User input: search
         """
 
         # implemented in sub classes
 
-    def _search_next(self, *args):
+    def _search_next(self, *args: Any) -> None:
         """
         User input: search for next match
         """
 
         # implemented in sub classes
 
-    def _search_prev(self, *args):
+    def _search_prev(self, *args: Any) -> None:
         """
         User input: search for previous match
         """
 
         # implemented in sub classes
 
-    def _enter(self, *args):
+    def _enter(self, *args: Any) -> None:
         """
         User input: enter
         """
 
         # implemented in sub classes
 
-    def _tab(self, *args):
+    def _tab(self, *args: Any) -> None:
         """
         User input: tab
         """
 
         # implemented in sub classes
 
-    def _quit(self, *args):
+    def _quit(self, *args: Any) -> None:
         """
         User input: quit nuqql
         """
 
         # implemented in sub classes
 
-    def handle_keybinds(self, *args, keybinds=None, keyfunc=None):
+    def handle_keybinds(self, *args: Any, keybinds: Dict[str, str] = None,
+                        keyfunc: Dict[str, Callable] = None) -> bool:
         """
         Handle special keys
         """
@@ -429,7 +436,7 @@ class Win:
         # no special key
         return False
 
-    def _init_keyfunc(self):
+    def _init_keyfunc(self) -> None:
         """
         Initialize key to function mapping
         """
