f4cb43589c9c0e3ec85f854181d12544a6586bff
hwipl <33433250+hwipl@users.noreply.github.com>
2019-12-17 15:45:50 +0100

    add --version command line argument
    Add the "--version" command line argument for querying nuqql's version.
    
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- nuqql/main.py
+++ nuqql/main.py
@@ -6,15 +6,30 @@ Main part of nuqql.
 # Main Part #
 #############
 
+import argparse
 import signal
 
 import nuqql.backend
 import nuqql.ui
 
+VERSION = "0.7"
+
 
 ###############
 # MAIN (LOOP) #
 ###############
+
+def parse_args() -> None:
+    """
+    Parse command line arguments.
+    """
+
+    # if we add more, consider moving it to config or somewhere else
+    parser = argparse.ArgumentParser(
+        description="Run nuqql command line instant messenger.")
+    parser.add_argument("--version", action="version", version=VERSION)
+    parser.parse_args()
+
 
 def main_loop() -> str:
     """
@@ -45,6 +60,9 @@ def run() -> None:
     """
     Main entry point of nuqql
     """
+
+    # parse command line arguments
+    parse_args()
 
     # ignore SIGINT
     signal.signal(signal.SIGINT, signal.SIG_IGN)
