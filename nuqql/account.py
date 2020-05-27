7982426af80123297aad778d81c78888a0b1c513
hwipl <33433250+hwipl@users.noreply.github.com>
2020-05-27 15:06:09 +0200

    move Account class into account.py
    Move the Account class into the new file account.py.
    
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

---
+++ nuqql/account.py
@@ -0,0 +1,78 @@
+"""
+Nuqql account
+"""
+
+import time
+
+from typing import List, TYPE_CHECKING
+
+import nuqql.ui
+
+from nuqql.buddy import Buddy
+
+if TYPE_CHECKING:
+    # pylint: disable=cyclic-import
+    from nuqql.backend import Backend
+
+
+# update buddies only every BUDDY_UPDATE_TIMER seconds
+BUDDY_UPDATE_TIMER = 5
+
+
+class Account:
+    """
+    Class for Accounts
+    """
+
+    def __init__(self, aid: str, prot: str, user: str) -> None:
+        self.aid = aid
+        self.name = user
+        self.type = prot
+        self.buddies: List[Buddy] = []
+        self.buddies_update = 0
+
+    def update_buddies(self) -> bool:
+        """
+        Update the buddy list of this account.
+        Return True if an update is pending, False otherwise.
+        """
+
+        # update only once every BUDDY_UPDATE_TIMER seconds
+        if time.time() - self.buddies_update <= BUDDY_UPDATE_TIMER:
+            return False
+        self.buddies_update = int(time.time())
+
+        # remove buddies, that have not been updated for a while
+        for rem in [buddy for buddy in self.buddies if not buddy.updated]:
+            nuqql.ui.remove_buddy(rem)
+        self.buddies = [buddy for buddy in self.buddies if buddy.updated]
+
+        # set update pending in buddy
+        for buddy in self.buddies:
+            buddy.updated = False
+
+        return True
+
+    def update_buddy(self, backend: "Backend", name: str, alias: str,
+                     status: str) -> None:
+        """
+        Update a single buddy of this account. Could be a new buddy.
+        """
+
+        # look for existing buddy
+        for buddy in self.buddies:
+            if buddy.name == name:
+                if buddy.update(status, alias):
+                    # tell ui about the update
+                    nuqql.ui.update_buddy(buddy)
+
+                # found existing buddy; stop here
+                return
+
+        # new buddy
+        new_buddy = Buddy(backend, self, name)
+        new_buddy.update(status, alias)
+        self.buddies.append(new_buddy)
+
+        # tell ui there is a new buddy
+        nuqql.ui.add_buddy(new_buddy)
