f259944414d2f0c9e61c04a7b22d7a35cda6e120
hwipl <33433250+hwipl@users.noreply.github.com>
2020-06-06 18:30:01 +0200

    write backend subprocess output to nuqql.log
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- nuqql/backend/server.py
+++ nuqql/backend/server.py
@@ -4,9 +4,10 @@ Backend server
 
 import logging
 import subprocess
+import threading
 
 from pathlib import Path
-from typing import Optional, TextIO
+from typing import Optional
 
 # enable/disable logging of subprocess output
 SUBPROCESS_LOGGING = True
@@ -23,9 +24,30 @@ class BackendServer:
         self.server_path = path
         self.server_cmd = cmd
 
-        # subprocess output logging files
-        self.stdout_file: Optional[TextIO] = None
-        self.stderr_file: Optional[TextIO] = None
+        # subprocess output logging thread
+        self.output_logger: Optional[threading.Thread] = None
+        self.stop_logger = threading.Event()
+
+    def _start_logging(self) -> None:
+        """
+        Start logging the backend server's output to the nuqql log
+        """
+
+        name = self.server_cmd.split()[0]
+
+        def logger() -> None:
+            assert self.proc and self.proc.stdout
+            while not self.stop_logger.is_set():
+                try:
+                    for line in iter(self.proc.stdout.readline, b''):
+                        logging.debug("BackendServer: got backend subprocess "
+                                      "%s output:\n %s", name, line.decode())
+                except OSError:
+                    logging.error("BackendServer: error reading subprocess %s"
+                                  "output (read)", name)
+                    return
+        self.output_logger = threading.Thread(target=logger)
+        self.output_logger.start()
 
     def start(self) -> None:
         """
@@ -37,23 +59,25 @@ class BackendServer:
         # make sure server's working directory exists
         Path(self.server_path).mkdir(parents=True, exist_ok=True)
 
-        # if logging is enabled for subprocess output, open log files
+        # if logging is enabled for subprocess use pipes
+        stdout = subprocess.DEVNULL
+        stderr = subprocess.DEVNULL
         if SUBPROCESS_LOGGING:
-            Path(self.server_path + "/logs").mkdir(parents=True, exist_ok=True)
-            self.stdout_file = open(self.server_path +
-                                    "/logs/backend-stdout.log", "a")
-            self.stderr_file = open(self.server_path +
-                                    "/logs/backend-stderr.log", "a")
+            stdout = subprocess.PIPE
+            stderr = subprocess.STDOUT
 
         # start server process
         self.proc = subprocess.Popen(
             self.server_cmd,
             shell=True,
-            stdout=subprocess.DEVNULL,
-            stderr=subprocess.DEVNULL,
+            stdout=stdout,
+            stderr=stderr,
             start_new_session=True,     # dont send SIGINT from nuqql to
                                         # subprocess
         )
+
+        if SUBPROCESS_LOGGING:
+            self._start_logging()
 
     def stop(self) -> None:
         """
@@ -66,9 +90,7 @@ class BackendServer:
         if self.proc:
             self.proc.terminate()
 
-        # close subprocess log files if logging is enabled
-        if SUBPROCESS_LOGGING:
-            if self.stdout_file:
-                self.stdout_file.close()
-            if self.stderr_file:
-                self.stderr_file.close()
+        # stop output logger
+        if self.output_logger:
+            self.stop_logger.set()
+            self.output_logger.join()
